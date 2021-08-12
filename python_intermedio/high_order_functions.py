0233ef70cffdcf2c64b07d75e31cb6e6379772c4
Kate <leandrokategora@gmail.com>
2021-08-12 20:26:56 -0300

    aprendiendo high order functions, estan las 3 en un solo archivo

---
+++ python_intermedio/high_order_functions.py
@@ -0,0 +1,21 @@
+from functools import reduce
+
+def run():
+    #FILTER sirve para filtrar datos de un iterable y retornarlos en un nuevo iterable
+    my_list = [x for x in range(1, 111)
+    ]
+    odd = list(filter(lambda x: x % 2 != 0, my_list))
+    print("FILTER: ", odd)
+    
+    #MAP sirve para "procesar" o potenciar los elementos del iterable y retornarlos dentro de un nuevo iterable
+    squares = list(map(lambda x: x**2, my_list))
+    print("MAP: ", squares)
+
+
+    #REDUCE sirve para multiplicar los elementos del iterable, hasta obtener un solo elemento que se retorna en un nuevo iterable
+    all_multiplied = reduce(lambda a, b: a * b, my_list)
+    print("REDUCE: ", all_multiplied)
+
+
+if __name__ == '__main__':
+    run()    
