c8b6326e33dedab2c8adce5bf50ae198fdec9c2c
Kate <leandrokategora@gmail.com>
2021-08-13 19:35:28 -0300

    ultimo commit antes de empezar con el filtrado de datos

--- python_intermedio/lambda_functions.py
+++ python_intermedio/lambda_functions.py
@@ -1,6 +1,7 @@
 def run():
 
-    #nombre de la variable: palindrome. 
+    #nombre de la variable: variable que identifica a la funcion, palindrome.
+    # palindrime seria un objeto o variable que recibe como parametro a la funcion de adentro 
     #palabra clave: lambda
     #argumento: string, accion: string = string dado vuelta?
     #esto deberia retornar un valor true o false, para cada 
@@ -8,7 +9,7 @@ def run():
     # que se utiliza como identificador y arguemnto de la funcion 
     palindrome = lambda string: string == string[::-1]
     print(palindrome(input("ingrese una palabra:")))
-
+ 
 
 if __name__=='__main__':
     run()
