08eedc71cc07d32312bac4a4068c25f7465ead8b
Kate <leandrokategora@gmail.com>
2021-08-23 16:41:59 -0300

    este commit es para guardar el archivo Java con el ejercicio de fabian

---
+++ prueba_java.java
@@ -0,0 +1,10 @@
+List<string>ejemplo_lista = new ArrayList<String>();
+    ejemplo_lista.add("mango");
+    ejemplo_lista.add("banana");
+    ejemplo_lista.add("manzana");
+    ejemplo_lista.add("mango");
+    ejemplo_lista.add("pera");
+    ejemplo_lista.add("naranja");
+
+
+    ejemplo_lista.forEach((n) -> System.out.printIn(n));
