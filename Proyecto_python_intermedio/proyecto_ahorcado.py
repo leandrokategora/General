5c57a0ba2461269b328d19346a900a81f160711d
Kate <leandrokategora@gmail.com>
2021-08-31 15:08:50 -0300

    empece con el proyecto final del curso intermedio de python

---
+++ Proyecto_python_intermedio/proyecto_ahorcado.py
@@ -0,0 +1,31 @@
+from os import name
+
+
+def read():
+    numbers = []
+    with open("./numeros.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            numbers.append(int(line))
+
+    print(numbers)
+
+
+
+def write():
+    names = ["facundo", "miguel", "pepe", "christian"]
+    with open("./names.txt", "w", encoding="utf-8") as f:
+        for name in names:
+            f.write(name)
+            f.write("\n")
+            print()
+
+
+def run():
+    read()
+    write()
+
+
+
+
+if __name__ == "__main__":
+    run()
