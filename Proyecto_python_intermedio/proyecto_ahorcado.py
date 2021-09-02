34351d9298b4b9dd9b0aabe48444f483880017a7
Kate <leandrokategora@gmail.com>
2021-09-02 16:21:52 -0300

    este commit no lo llegue a terminar porque me quede sin tiempo, continuo en casa

--- Proyecto_python_intermedio/proyecto_ahorcado.py
+++ Proyecto_python_intermedio/proyecto_ahorcado.py
@@ -1,29 +1,39 @@
-from os import name
+from os import name 
+from random import random
+
+# def read():
+#     numbers = []
+#     with open("./numeros.txt", "r", encoding="utf-8") as f:
+#         for line in f:
+#             numbers.append(int(line))
+
+#     print(numbers)
 
 
-def read():
-    numbers = []
-    with open("./numeros.txt", "r", encoding="utf-8") as f:
+
+# def write():
+#     names = ["facundo", "miguel", "pepe", "christian"]
+#     with open("./names.txt", "w", encoding="utf-8") as f:
+#         for name in names:
+#             f.write(name)
+#             f.write("\n")
+#             print()
+
+def list_1():
+    lista_de_palabras = []
+    with open("./data.txt", "r", encoding="utf-8") as f:
         for line in f:
-            numbers.append(int(line))
+            lista_de_palabras.append(line)
 
-    print(numbers)
-
-
-
-def write():
-    names = ["facundo", "miguel", "pepe", "christian"]
-    with open("./names.txt", "w", encoding="utf-8") as f:
-        for name in names:
-            f.write(name)
-            f.write("\n")
-            print()
+    print(lista_de_palabras)
+    
+def palabra(a):
+    a = 
 
 
 def run():
-    read()
-    write()
-#necesito ver porque no sube esto al repositorio remoto
+    list_1()
+    
 
 
 
