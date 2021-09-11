3c1d9e49ec0c3aef49cf6c96db16e877792c95c5
Kate <leandrokategora@gmail.com>
2021-09-11 19:25:27 -0300

    empece con el proyecto de procesar datos con python desde un archivo externo

---
+++ Proyecto_python_intermedio/proyecto_filtrar.py
@@ -0,0 +1,25 @@
+from proyecto_ahorcado import list_1
+
+
+with open("./data.txt", "r", encoding="utf-8") as f:
+    list1 = [palabra for palabra in f]
+
+with open("./VerCheckinCheckout.csv", "r", encoding="UTF-8") as e:    
+    list2 = [line for line in e]
+    # for line in e:
+    #     list2.append(line)
+# my_dictx= {i: i+1 for i in range(list2.len)}
+
+
+def run():
+    print(list2)
+    for elemento in list2[1::]:
+        print(elemento)        
+    # print(my_dictx)
+
+
+
+
+
+if __name__=="__main__":
+    run()
