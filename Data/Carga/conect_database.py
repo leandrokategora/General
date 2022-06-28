e90f2b490b5ab6f7a64fb94902af0cb5b77d6fba
Kate <leandrokategora@gmail.com>
2022-06-27 23:09:10 -0300

    error al configurar el conector a la bd

--- Data/Carga/conect_database.py
+++ Data/Carga/conect_database.py
@@ -1,14 +1,28 @@
+# import sqlalchemy as sa
+# import urllib
+# parameters = urllib.parse.quote_plus("DRIVER={mysql};"
+#                                      "Server=127.0.0.1;"
+#                                      "DATABASE=rkd_challenge;"
+#                                      "UID=root@localhost;"
+#                                      "PWD=")
+
+# try:
+#     engine = sa.create_engine("mysql+pyodbc:///?odbc_conect={}".format(parameters))
+#     print('Conexion exitosa')
+# except Exception as ex:
+#     print(ex)
+    
 import sqlalchemy as sa
 import urllib
-parameters = urllib.parse.quote_plus("DRIVER={mysql};"
+import pyodbc
+parameters = urllib.parse.quote_plus("DRIVER=MySQL ODBC 8.0 Unicode Driver;"
                                      "Server=127.0.0.1;"
                                      "DATABASE=rkd_challenge;"
-                                     "UID=root@localhost;"
+                                     "UID=root;"
                                      "PWD=")
 
 try:
     engine = sa.create_engine("mysql+pyodbc:///?odbc_conect={}".format(parameters))
     print('Conexion exitosa')
 except Exception as ex:
-    print(ex)
-    
+    print(ex)    
